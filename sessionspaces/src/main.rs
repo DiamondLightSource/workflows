257a637e72bc5bb838a2759a2eadf5b5b7d431d4
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-05-02 14:18:34 +0000

    Add Session permissionable from ISPyB

--- sessionspaces/src/main.rs
+++ sessionspaces/src/main.rs
@@ -3,6 +3,8 @@
 #![warn(clippy::missing_docs_in_private_items)]
 #![doc = include_str!("../README.md")]
 
+/// ISPyB permissionables tables
+mod permissionables;
 /// Kubernetes resource templating
 mod resources;
 
---
+++ sessionspaces/src/permissionables/mod.rs
@@ -0,0 +1,4 @@
+/// Beamline sessions
+mod session;
+
+pub use self::session::Session;
