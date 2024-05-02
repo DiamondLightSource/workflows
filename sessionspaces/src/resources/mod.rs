59bdcbc41eb38dc9622aef0ed4f6ef54b870bf86
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-05-02 15:03:34 +0100

    Add K8s session member ServiceAccount resource management

--- sessionspaces/src/resources/mod.rs
+++ sessionspaces/src/resources/mod.rs
@@ -1,9 +1,12 @@
+/// The visit-member ServiceAccount and corresponding Role
+mod member_sa;
 /// The Namespace for a beamline session
 mod namespace;
 /// The argo-workflows ServiceAccount and corresponding Role
 mod workflows_sa;
 
 pub use self::{
+    member_sa::{create_visit_member_role, create_visit_member_service_account},
     namespace::{create_namespace, delete_namespace},
     workflows_sa::{create_argo_workflows_role, create_argo_workflows_service_account},
 };
