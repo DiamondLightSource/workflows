b03b1e30715e1809949769659772aad41c3b4aeb
iamvigneshwars <iamvigneshwars@gmail.com>
2024-05-24 17:14:35 +0000

    Test DB fetch

---
+++ sessionspaces/tests/fixtures/proposals.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    `Proposal` (
+        `proposalId`,
+        `proposalNumber`,
+        `proposalCode`,
+    )
+VALUES 
+    (30, "10030", "sw"),
+    (31, "10031", "sw"),
+    (32, "10032", "cm")
