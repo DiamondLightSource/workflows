8d88dd818f62d0fcab9e943c4b18d166bab4ae97
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-06-12 16:30:37 +0000

    Further simplify sessionspaces ConfigMap creation

--- sessionspaces/src/resources/config_maps.rs
+++ sessionspaces/src/resources/config_maps.rs
@@ -1,11 +1,12 @@
 use super::{MANAGED_BY, MANAGED_BY_LABEL};
+use crate::Sessionspace;
 use itertools::Itertools;
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::{
     api::{ObjectMeta, Patch, PatchParams},
     Api, Client,
 };
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 use tracing::{info, instrument};
 
 /// The name to be given to the ConfigMap
@@ -13,28 +14,29 @@ const NAME: &str = "sessionspaces";
 
 #[instrument(skip(k8s_client))]
 pub async fn create_configmap(
-    namespace: String,
-    session_code: String,
-    session_number: u32,
-    visit_number: u32,
-    gid_number: Option<String>,
-    members: BTreeSet<String>,
+    namespace: &str,
+    sessionspace: Sessionspace,
     k8s_client: Client,
 ) -> std::result::Result<(), kube::Error> {
-    let configmaps = Api::<ConfigMap>::namespaced(k8s_client, &namespace);
-    let mut configmap_data = BTreeMap::from([
-        ("session_code".to_string(), session_code),
-        ("session_number".to_string(), session_number.to_string()),
-        ("visit_number".to_string(), visit_number.to_string()),
-    ]);
-    configmap_data.insert(
-        "members".to_string(),
-        format!(
-            "[{}]",
-            members.iter().map(|s| format!("\"{}\"", s)).join(", ")
-        ),
+    let configmaps = Api::<ConfigMap>::namespaced(k8s_client, namespace);
+    let members = format!(
+        "[{}]",
+        sessionspace
+            .members
+            .iter()
+            .map(|s| format!(r#""{}""#, s))
+            .join(", ")
     );
-    if let Some(gid) = gid_number {
+    let mut configmap_data = BTreeMap::from([
+        ("proposal_code".to_string(), sessionspace.proposal_code),
+        (
+            "proposal_number".to_string(),
+            sessionspace.proposal_number.to_string(),
+        ),
+        ("visit".to_string(), sessionspace.visit.to_string()),
+        ("members".to_string(), members),
+    ]);
+    if let Some(gid) = sessionspace.gid {
         configmap_data.insert("gid".to_string(), gid);
     }
     configmaps
