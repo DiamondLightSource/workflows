17e8beecfb457a22a56a493d564fe3c1c6b640ee
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-06-13 08:50:49 +0000

    Use serde_json to produce member list

--- sessionspaces/src/resources/config_maps.rs
+++ sessionspaces/src/resources/config_maps.rs
@@ -1,6 +1,5 @@
 use super::{MANAGED_BY, MANAGED_BY_LABEL};
 use crate::Sessionspace;
-use itertools::Itertools;
 use k8s_openapi::api::core::v1::ConfigMap;
 use kube::{
     api::{ObjectMeta, Patch, PatchParams},
@@ -17,16 +16,8 @@ pub async fn create_configmap(
     namespace: &str,
     sessionspace: Sessionspace,
     k8s_client: Client,
-) -> std::result::Result<(), kube::Error> {
+) -> std::result::Result<(), anyhow::Error> {
     let configmaps = Api::<ConfigMap>::namespaced(k8s_client, namespace);
-    let members = format!(
-        "[{}]",
-        sessionspace
-            .members
-            .iter()
-            .map(|s| format!(r#""{}""#, s))
-            .join(", ")
-    );
     let mut configmap_data = BTreeMap::from([
         ("proposal_code".to_string(), sessionspace.proposal_code),
         (
@@ -35,7 +26,10 @@ pub async fn create_configmap(
         ),
         ("visit".to_string(), sessionspace.visit.to_string()),
         ("beamline".to_string(), sessionspace.beamline),
-        ("members".to_string(), members),
+        (
+            "members".to_string(),
+            serde_json::to_string(&sessionspace.members)?,
+        ),
     ]);
     if let Some(gid) = sessionspace.gid {
         configmap_data.insert("gid".to_string(), gid);
