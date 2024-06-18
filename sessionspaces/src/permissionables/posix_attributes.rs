f652e46f5d9efd299914aeee29d8a2c4eaf3b3c2
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-06-18 10:47:50 +0000

    Fetch posix_attributes as mapping

--- sessionspaces/src/permissionables/posix_attributes.rs
+++ sessionspaces/src/permissionables/posix_attributes.rs
@@ -1,20 +1,21 @@
+use derive_more::{Deref, DerefMut};
 use ldap3::{Ldap, LdapError, Scope, SearchEntry};
 use std::collections::BTreeMap;
 
+/// A mapping of sessions to their posix attributes
+#[derive(Debug, Default, Deref, DerefMut)]
+pub struct SessionPosixAttributes(BTreeMap<String, PosixAttributes>);
+
 /// The posix attributes of a beamline session group
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct PosixAttributes {
-    /// The unique identifier of the session
-    pub session_name: String,
     /// The posix Group ID of the session group
     pub gid: String,
 }
 
-impl PosixAttributes {
+impl SessionPosixAttributes {
     /// Fetches [`PosixAttributes`] from the SciComp LDAP
-    pub async fn fetch(
-        ldap_connection: &mut Ldap,
-    ) -> Result<BTreeMap<String, PosixAttributes>, LdapError> {
+    pub async fn fetch(ldap_connection: &mut Ldap) -> Result<Self, LdapError> {
         let (rs, _res) = ldap_connection
             .search(
                 "ou=Group,dc=diamond,dc=ac,dc=uk",
@@ -25,22 +26,22 @@ impl PosixAttributes {
             .await?
             .success()?;
 
-        let mut posix_attr = BTreeMap::new();
-        for entry in rs {
-            if let Some(gid) = SearchEntry::construct(entry.clone()).attrs.get("gidNumber") {
-                if let Some(session_name) = SearchEntry::construct(entry.clone()).attrs.get("cn") {
-                    let session_name_str = session_name.concat().replace('_', "-");
-
-                    posix_attr.insert(
-                        session_name_str.clone(),
-                        PosixAttributes {
-                            session_name: session_name_str,
-                            gid: gid.concat(),
-                        },
-                    );
+        let mut posix_attributes = Self::default();
+        for result_entry in rs {
+            let mut search_entry = SearchEntry::construct(result_entry);
+            if let Some(Ok([gid])) = search_entry
+                .attrs
+                .remove("gidNumber")
+                .map(<[_; 1]>::try_from)
+            {
+                if let Some(Ok([session_name])) =
+                    search_entry.attrs.remove("cn").map(<[_; 1]>::try_from)
+                {
+                    posix_attributes
+                        .insert(session_name.replace('_', "-"), PosixAttributes { gid });
                 }
             }
         }
-        Ok(posix_attr)
+        Ok(posix_attributes)
     }
 }
