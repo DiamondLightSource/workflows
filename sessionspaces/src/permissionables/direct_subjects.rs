519186007f5c620707fb3a34f13471256db1513e
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-06-18 15:55:50 +0000

    Rename session subjects to direct subjects

--- sessionspaces/src/permissionables/direct_subjects.rs
+++ sessionspaces/src/permissionables/direct_subjects.rs
@@ -5,21 +5,21 @@ use tracing::instrument;
 
 /// A mapping of sessions to the subjects on them
 #[derive(Debug, Default, Deref, DerefMut, PartialEq, Eq, PartialOrd, Ord)]
-pub struct SessionSubjects(BTreeMap<u32, BTreeSet<String>>);
+pub struct DirectSubjects(BTreeMap<u32, BTreeSet<String>>);
 
 #[allow(clippy::missing_docs_in_private_items)]
-struct SubjectRow {
+struct DirectSubjectRow {
     subject: Option<String>,
     session: u32,
 }
 
-impl SessionSubjects {
-    /// Fetches [`SessionSubjects`]s from ISPyB
-    #[instrument(name = "fetch_subject_sessions")]
-    pub async fn fetch(ispyb_pool: &MySqlPool) -> Result<Self, anyhow::Error> {
-        let mut session_subjects = Self::default();
-        for SubjectRow { session, subject } in query_as!(
-            SubjectRow,
+impl DirectSubjects {
+    /// Fetches [`DirectSubjects`] from ISPyB
+    #[instrument(name = "fetch_direct_subjects")]
+    pub async fn fetch(ispyb_pool: &MySqlPool) -> Result<Self, sqlx::Error> {
+        let mut direct_subjects = Self::default();
+        for DirectSubjectRow { session, subject } in query_as!(
+            DirectSubjectRow,
             "
             SELECT
                 sessionId as session,
@@ -33,27 +33,27 @@ impl SessionSubjects {
         .await?
         {
             if let Some(subject) = subject {
-                session_subjects
+                direct_subjects
                     .entry(session)
                     .or_insert(BTreeSet::default())
                     .insert(subject);
             }
         }
-        Ok(session_subjects)
+        Ok(direct_subjects)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::SessionSubjects;
+    use super::DirectSubjects;
     use sqlx::MySqlPool;
     use std::collections::{BTreeMap, BTreeSet};
 
     #[sqlx::test(migrations = "tests/migrations")]
     async fn fetch_empty(ispyb_pool: MySqlPool) {
-        let subject_sessions = SessionSubjects::fetch(&ispyb_pool).await.unwrap();
-        let expected = SessionSubjects::default();
-        assert_eq!(expected, subject_sessions);
+        let direct_subjects = DirectSubjects::fetch(&ispyb_pool).await.unwrap();
+        let expected = DirectSubjects::default();
+        assert_eq!(expected, direct_subjects);
     }
 
     #[sqlx::test(
@@ -64,14 +64,14 @@ mod tests {
         )
     )]
     async fn fetch_some(ispyb_pool: MySqlPool) {
-        let subject_sessions = SessionSubjects::fetch(&ispyb_pool).await.unwrap();
-        let expected = SessionSubjects(BTreeMap::from([
+        let direct_subjects = DirectSubjects::fetch(&ispyb_pool).await.unwrap();
+        let expected = DirectSubjects(BTreeMap::from([
             (40, BTreeSet::from(["foo".to_string()])),
             (41, BTreeSet::from(["foo".to_string()])),
             (42, BTreeSet::from(["foo".to_string()])),
             (43, BTreeSet::from(["bar".to_string()])),
             (44, BTreeSet::from(["bar".to_string()])),
         ]));
-        assert_eq!(expected, subject_sessions);
+        assert_eq!(expected, direct_subjects);
     }
 }
