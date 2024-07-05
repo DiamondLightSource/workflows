96400236ae23342c182268f6821fb12795e31772
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-07-05 13:37:04 +0000

    Patch namespaces during sessionspaces update

--- sessionspaces/src/resources/namespace.rs
+++ sessionspaces/src/resources/namespace.rs
@@ -1,8 +1,9 @@
 use super::{MANAGED_BY, MANAGED_BY_LABEL};
 use k8s_openapi::api::core::v1::Namespace;
+use kube::api::{Patch, PatchParams};
 use kube::Error as KubeError;
 use kube::{
-    api::{DeleteParams, ObjectMeta, PostParams},
+    api::{DeleteParams, ObjectMeta},
     Api,
 };
 use std::collections::BTreeMap;
@@ -35,31 +36,24 @@ pub async fn create_namespace(
     k8s_client: kube::Client,
 ) -> Result<(), kube::Error> {
     let api = Api::<Namespace>::all(k8s_client.clone());
-    match api.get(&namespace.clone()).await {
-        Ok(_) => {
-            info!("Namespace {namespace} already exists, skipping creation");
-            Ok(())
-        }
-        Err(KubeError::Api(api_err)) if api_err.code == 404 => {
-            api.create(
-                &PostParams::default(),
-                &Namespace {
-                    metadata: ObjectMeta {
-                        name: Some(namespace.clone()),
-                        labels: Some(BTreeMap::from([(
-                            MANAGED_BY_LABEL.to_string(),
-                            MANAGED_BY.to_string(),
-                        )])),
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                },
-            )
-            .await?;
-            Ok(())
-        }
-        Err(e) => Err(e),
-    }
+    api.patch(
+        &namespace,
+        &PatchParams::default(),
+        &Patch::Apply(&Namespace {
+            metadata: ObjectMeta {
+                name: Some(namespace.clone()),
+                labels: Some(BTreeMap::from([(
+                    MANAGED_BY_LABEL.to_string(),
+                    MANAGED_BY.to_string(),
+                )])),
+                ..Default::default()
+            },
+            ..Default::default()
+        }),
+    )
+    .await?;
+    info!("Namespace {namespace} created / updated");
+    Ok(())
 }
 
 #[cfg(test)]
@@ -70,90 +64,27 @@ mod tests {
     use mockito::Server;
 
     #[tokio::test]
-    async fn test_create_namespace_existing() {
-        // Create a mock server
+    async fn create_new_namespace() {
         let mut server = Server::new_async().await;
-
-        let m1 = server
-            .mock("GET", "/api/v1/namespaces/test-namespace1")
-            .with_status(200)
-            .with_body(
-                r#"{
-                    "kind": "Namespace",
-                    "apiVersion": "v1",
-                    "metadata": {
-                        "name": "test-namespace1"
-                    }
-                }"#,
-            )
-            .create();
-
-        let m2 = server
-            .mock("GET", "/api/v1/namespaces/test-namespace2")
-            .with_status(200)
-            .with_body(
-                r#"{
-                    "kind": "Namespace",
-                    "apiVersion": "v1",
-                    "metadata": {
-                        "name": "test-namespace2"
-                    }
-                }"#,
-            )
-            .create();
-
-        let config = Config::new(server.url().parse().unwrap());
-        let k8s_client = Client::try_from(config).unwrap();
-
-        assert!(
-            create_namespace("test-namespace1".to_string(), k8s_client.clone())
-                .await
-                .is_ok()
-        );
-        assert!(create_namespace("test-namespace2".to_string(), k8s_client)
-            .await
-            .is_ok());
-
-        // Verify expectations
-        m1.assert();
-        m2.assert();
-    }
-
-    #[tokio::test]
-    async fn test_create_namespace() {
-        // Create a mock server
-        let mut server = Server::new_async().await;
-
-        let m1 = server
-            .mock("POST", "/api/v1/namespaces?")
+        let mock_patch_test_namespace = server
+            .mock("PATCH", "/api/v1/namespaces/test?")
             .with_status(201)
             .with_header("content-type", "application/json")
             .with_body(
                 r#"{
-                    "kind": "Namespace",
                     "apiVersion": "v1",
+                    "kind": "Namespace",
                     "metadata": {
-                        "name": "new-namespace"
+                        "name": "test"
                     }
                 }"#,
             )
             .create();
-
-        let m2 = server
-            .mock("GET", "/api/v1/namespaces/testing-namespace3")
-            .with_status(404)
-            .create();
-
         let config = Config::new(server.url().parse().unwrap());
         let k8s_client = Client::try_from(config).unwrap();
-        assert!(
-            create_namespace("testing-namespace3".to_string(), k8s_client)
-                .await
-                .is_ok()
-        );
-
-        // Verify if the request hits the mock endpoints
-        m1.assert();
-        m2.assert();
+        create_namespace("test".to_string(), k8s_client)
+            .await
+            .unwrap();
+        mock_patch_test_namespace.assert();
     }
 }
