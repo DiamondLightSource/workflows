97b7a4ed81a7c55671ca79086f722900b130f790
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-13 15:24:59 +0000

    update package to lib

--- UI/workflows-components/vite.config.ts
+++ UI/workflows-components/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      name: "WorkflowLibrary",
+      fileName: "workflow-library",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "@mui/material"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
