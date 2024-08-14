7fb474fbc685f6c50f856db1a626c8facaf674b0
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-14 15:07:48 +0000

    update library structure, add yarn workspace

--- UI/workflows-lib/eslint.config.js
+++ UI/workflows-lib/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
   extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked],
@@ -11,10 +11,6 @@ export default tseslint.config({
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
-    parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
   },
   plugins: {
     'react-hooks': reactHooks,
