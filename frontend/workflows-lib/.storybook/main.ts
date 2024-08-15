54772c0867416d63713dcccc7ad9c21a9be012a7
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-15 11:23:34 +0000

    supress linting errors for storybook setup files

--- frontend/workflows-lib/.storybook/main.ts
+++ frontend/workflows-lib/.storybook/main.ts
@@ -6,6 +6,7 @@ import { join, dirname } from "path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
@@ -22,6 +23,7 @@ const config: StorybookConfig = {
     getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
