d1adeb6bed27cf220d9e260c0b4192d82298fb63
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-15 09:51:08 +0000

    add parseroption for eslint

--- UI/workflows-lib/eslint.config.js
+++ UI/workflows-lib/eslint.config.js
@@ -11,6 +11,10 @@ export default tseslint.config({
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
   plugins: {
     'react-hooks': reactHooks,
