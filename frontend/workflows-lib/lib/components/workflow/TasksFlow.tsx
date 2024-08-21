3849198ba46406fd9e2e162ab072b47d6cba490b
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-21 14:54:05 +0000

    add buildTaskTree function to TasksFlow component

--- frontend/workflows-lib/lib/components/workflow/TasksFlow.tsx
+++ frontend/workflows-lib/lib/components/workflow/TasksFlow.tsx
@@ -4,11 +4,37 @@ import "@xyflow/react/dist/style.css";
 import React from "react";
 import { Task } from "../../types";
 
+interface TaskNode extends Task {
+  children?: TaskNode[];
+}
+
+const buildTaskTree = (tasks: Task[]): TaskNode[] => {
+  const taskMap: { [key: string]: TaskNode } = {};
+  const roots: TaskNode[] = [];
+
+  tasks.forEach((task) => {
+    taskMap[task.name] = { ...task, children: [] };
+  });
+
+  tasks.forEach((task) => {
+    if (task.depends) {
+      taskMap[task.depends].children?.push(taskMap[task.name]);
+    } else {
+      roots.push(taskMap[task.name]);
+    }
+  });
+
+  return roots;
+};
+
 interface TasksFlowProps {
   tasks: Task[];
 }
 
-const TasksFlow: React.FC<TasksFlowProps> = () => {
+const TasksFlow: React.FC<TasksFlowProps> = ({ tasks }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const taskTree = buildTaskTree(tasks);
+
   return (
     <Box display="flex" height="50vh" width="100%">
       <ReactFlow
