5f14d3bbeb64aeec8dc009fca55bf60978f81f17
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-21 14:22:32 +0000

    create a separate directory for mock data

---
+++ frontend/workflows-lib/stories/mockData/Tasks.ts
@@ -0,0 +1,54 @@
+export const fakeTasks = [
+  {
+    id: "1",
+    workflow_id: "1",
+    name: "task-1",
+    status: "completed",
+  },
+  {
+    id: "2",
+    depends: "1",
+    workflow_id: "1",
+    name: "task-2 DKJFOKJLSKDMFODSJFLKSDJFLKSD",
+    status: "completed",
+  },
+  {
+    id: "3",
+    depends: "1",
+    workflow_id: "1",
+    name: "task-3",
+    status: "pending",
+  },
+  {
+    id: "4",
+    workflow_id: "1",
+    name: "task-4 ERFBAK3KJ34",
+    status: "running",
+  },
+  {
+    id: "5",
+    workflow_id: "1",
+    name: "task-5 EOI909D",
+    status: "pending",
+  },
+  {
+    id: "6",
+    depends: "2",
+    workflow_id: "1",
+    name: "task-6 KNMNE9",
+    status: "running",
+  },
+  {
+    id: "7",
+    workflow_id: "1",
+    name: "task-7 KLDJF034 DFJSOID 039402KDJO",
+    status: "running",
+  },
+  {
+    id: "8",
+    depends: "3",
+    workflow_id: "1",
+    name: "task-8",
+    status: "pending",
+  },
+];
