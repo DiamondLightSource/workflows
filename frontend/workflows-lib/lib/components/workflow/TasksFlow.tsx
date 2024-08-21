0ad774e7874ffa7ce9a9b21fed7a9bce01b3d03c
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-21 14:54:05 +0000

    add buildTaskTree function to TasksFlow component

--- frontend/workflows-lib/lib/components/workflow/TasksFlow.tsx
+++ frontend/workflows-lib/lib/components/workflow/TasksFlow.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { ReactFlow } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import React from "react";
+import React, { useMemo } from "react";
 
 interface Task {
   id: string;
@@ -11,11 +11,37 @@ interface Task {
   depends?: string;
 }
 
+interface TaskNode extends Task {
+  children?: TaskNode[];
+}
+
+const buildTaskTree = (tasks: Task[]): TaskNode[] => {
+  const taskMap: { [key: string]: TaskNode } = {};
+  const roots: TaskNode[] = [];
+
+  tasks.forEach((task) => {
+    taskMap[task.id] = { ...task, children: [] };
+  });
+
+  tasks.forEach((task) => {
+    if (task.depends) {
+      taskMap[task.depends].children?.push(taskMap[task.id]);
+    } else {
+      roots.push(taskMap[task.id]);
+    }
+  });
+
+  return roots;
+};
+
 interface TasksFlowProps {
   tasks: Task[];
 }
 
-const TasksFlow: React.FC<TasksFlowProps> = () => {
+const TasksFlow: React.FC<TasksFlowProps> = ({ tasks }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const taskTree = useMemo(() => buildTaskTree(tasks), [tasks]);
+
   return (
     <Box display="flex" height="50vh" width="100%">
       <ReactFlow
