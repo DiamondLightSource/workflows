c3e81cf7296931ebf88f68f5f24ff2e1417a0e95
iamvigneshwars <iamvigneshwars@gmail.com>
2024-08-22 12:11:55 +0000

    remove onboarding module for storybook, add fitview for tasks graph component

--- frontend/workflows-lib/.storybook/main.ts
+++ frontend/workflows-lib/.storybook/main.ts
@@ -16,7 +16,6 @@ const config: StorybookConfig = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
