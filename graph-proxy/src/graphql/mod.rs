487bab52d4e44c72d162b15cf89a7c60ba3eb812
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-08-28 10:57:16 +0000

    Add simple workflow resolver to graphql API

--- graph-proxy/src/graphql/mod.rs
+++ graph-proxy/src/graphql/mod.rs
@@ -1,3 +1,6 @@
+/// GraphQL operations related to workflows
+mod workflows;
+
 use async_graphql::{EmptyMutation, EmptySubscription, MergedObject, Schema, SchemaBuilder};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::extract::State;
@@ -5,25 +8,27 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
+use workflows::WorkflowsQuery;
 
 /// The root schema of the service
 pub type RootSchema = Schema<Query, EmptyMutation, EmptySubscription>;
 
 /// A schema builder for the service
 pub fn root_schema_builder() -> SchemaBuilder<Query, EmptyMutation, EmptySubscription> {
-    Schema::build(Query, EmptyMutation, EmptySubscription).enable_federation()
+    Schema::build(Query::default(), EmptyMutation, EmptySubscription).enable_federation()
 }
 
 /// The root query of the service
 #[derive(Debug, Clone, Default, MergedObject)]
-pub struct Query;
+pub struct Query(WorkflowsQuery);
 
 /// Handles HTTP requests as GraphQL according to the provided [`Schema`]
 pub async fn graphql_handler(
     State(schema): State<RootSchema>,
-    TypedHeader(auth_token): TypedHeader<Authorization<Bearer>>,
+    auth_token_header: Option<TypedHeader<Authorization<Bearer>>>,
     request: GraphQLRequest,
 ) -> GraphQLResponse {
+    let auth_token = auth_token_header.map(|header| header.0);
     schema
         .execute(request.into_inner().data(auth_token))
         .await
