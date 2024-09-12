834bfe1fc626f3eaa0f8902aeefa70ac0b6795c5
iamvigneshwars <iamvigneshwars@gmail.com>
2024-09-12 13:55:43 +0000

    add exports for workflow-lib components and types

---
+++ frontend/workflows-lib/lib/main.ts
@@ -0,0 +1,6 @@
+export { default as WorkflowAccordion } from "./components/workflow/WorkflowAccordian";
+export { default as WorkflowStack } from "./components/workflow/WorkflowStack";
+export { default as TasksFlow } from "./components/workflow/TasksFlow";
+export { default as TasksTable } from "./components/workflow/TasksTable";
+export { default as TasksDynamic } from "./components/workflow/TasksDynamic";
+export * from "./types";
