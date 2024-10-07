6f3002a11752810f8b81328abef76c032e0dea10
iamvigneshwars <iamvigneshwars@gmail.com>
2024-10-07 20:45:26 +0100

    add relay workflow component

---
+++ frontend/relay-workflows-lib/lib/components/WorkflowRelay.tsx
@@ -0,0 +1,40 @@
+import { useFragment } from "react-relay";
+import { graphql } from "react-relay";
+import { WorkflowAccordion } from "workflows-lib";
+import type { WorkflowStatus } from "workflows-lib";
+import { WorkflowRelayFragment$key } from "./__generated__/WorkflowRelayFragment.graphql";
+
+const WorkflowRelay = (props: {
+  workflow: WorkflowRelayFragment$key;
+  children: React.ReactNode;
+}) => {
+  const workflowFragment = graphql`
+    fragment WorkflowRelayFragment on Workflow {
+      name
+      status {
+        __typename
+      }
+    }
+  `;
+  const data = useFragment(workflowFragment, props.workflow);
+  const statusTextMap: { [key: string]: WorkflowStatus } = {
+    WorkflowPendingStatus: "Pending",
+    WorkflowRunningStatus: "Running",
+    WorkflowSucceededStatus: "Succeeded",
+    WorkflowFailedStatus: "Failed",
+    WorkflowErroredStatus: "Errored",
+  };
+  const statusText = statusTextMap[data.status.__typename];
+  return (
+    <WorkflowAccordion
+      workflow={{
+        name: data.name,
+        status: statusText,
+      }}
+    >
+      children
+    </WorkflowAccordion>
+  );
+};
+
+export default WorkflowRelay;
