43873045744f5abe8829c15d426c043d3048829a
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-10-08 08:41:33 +0000

    Add workflow templates query to graph-proxy

--- graph-proxy/src/graphql/workflow_templates.rs
+++ graph-proxy/src/graphql/workflow_templates.rs
@@ -66,4 +66,35 @@ impl WorkflowTemplatesQuery {
                 .into_result()?;
         Ok(workflow_templates.try_into()?)
     }
+
+    #[instrument(skip(self, ctx))]
+    async fn workflow_templates(&self, ctx: &Context<'_>) -> anyhow::Result<Vec<WorkflowTemplate>> {
+        let server_url = ctx.data_unchecked::<ArgoServerUrl>().deref();
+        let auth_token = ctx.data_unchecked::<Option<Authorization<Bearer>>>();
+        let mut url = server_url.clone();
+        url.path_segments_mut()
+            .unwrap()
+            .extend(["api", "v1", "cluster-workflow-templates"]);
+        debug!("Retrieving workflow templates from {url}");
+        let client = reqwest::Client::new();
+        let request = if let Some(auth_token) = auth_token {
+            client.get(url).bearer_auth(auth_token.token())
+        } else {
+            client.get(url)
+        };
+        let workflow_templates =
+            request
+                .send()
+                .await?
+                .json::<APIResult<
+                    argo_workflows_openapi::IoArgoprojWorkflowV1alpha1ClusterWorkflowTemplateList,
+                >>()
+                .await?
+                .into_result()?;
+        Ok(workflow_templates
+            .items
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect::<Result<Vec<_>, _>>()?)
+    }
 }
