aa2330e7b2f3bdc995f1f85b39c6a102f5bebe56
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2024-10-17 14:06:42 +0000

    Add submission form component

---
+++ frontend/workflows-lib/stories/SubmissionForm.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from "@storybook/react";
+import TemplateSubmissionForm from "../lib/components/template/SubmissionForm";
+
+const meta: Meta<typeof TemplateSubmissionForm> = {
+  title: "Submission Form",
+  component: TemplateSubmissionForm,
+};
+
+type Story = StoryObj<typeof TemplateSubmissionForm>;
+
+const fakeSchema = {
+  type: "object",
+  required: ["memory", "size"],
+  properties: {
+    memory: {
+      default: "20Gi",
+      type: "string",
+      pattern: "^[0-9]+[GMK]i$",
+    },
+    size: {
+      default: 2000,
+      type: "integer",
+    },
+  },
+};
+
+const fakeUiSchema = {
+  type: "VerticalLayout",
+  elements: [
+    {
+      type: "Control",
+      scope: "#/properties/memory",
+      label: "Memory",
+    },
+    {
+      type: "Control",
+      scope: "#/properties/size",
+      label: "Matrix Size",
+    },
+  ],
+};
+
+export default meta;
+export const Submission: Story = {
+  args: {
+    title: "Numpy Benchmark",
+    description: `
+        Runs a numpy script in a python container.
+        The script finds the normal of the dot product of two random matrices.
+        Matrix sizes are specified by the input parameter "size".
+      `,
+    parametersSchema: fakeSchema,
+    parametersUISchema: fakeUiSchema,
+    onSubmit: (visit, parameters) => {
+      alert(JSON.stringify({ visit, parameters }));
+    },
+  },
+};
