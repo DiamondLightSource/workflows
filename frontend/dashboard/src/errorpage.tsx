a39c144d2f3ea0035b1e8e2f6cd525e9d41c40a7
Benedikt Daurer <benedikt.daurer@diamond.ac.uk>
2024-11-13 12:21:35 +0000

    Added Router to the Workflows Dashboard app

---
+++ frontend/dashboard/src/errorpage.tsx
@@ -0,0 +1,27 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = (error.data as Error).message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error == 'string') {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = 'Unknown error';
+  }
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage}</i>
+      </p>
+    </div>
+  );
+}
--- frontend/dashboard/src/main.tsx
+++ frontend/dashboard/src/main.tsx
@@ -2,12 +2,30 @@ import { RelayEnvironmentProvider } from "react-relay";
 import { RelayEnvironment } from "./RelayEnvironment";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 import App from "./App.tsx";
+import WorkflowsList from "./routes/workflows.tsx";
+import ErrorPage from "./errorpage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "workflows",
+    element: <WorkflowsList />
+  }
+]);
 
 createRoot(document.getElementById("root") as Element).render(
   <RelayEnvironmentProvider environment={RelayEnvironment}>
     <StrictMode>
-      <App/>
+      <RouterProvider router={router} />
     </StrictMode>
   </RelayEnvironmentProvider>
 );
