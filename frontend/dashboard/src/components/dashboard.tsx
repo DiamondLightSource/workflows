bef92f47b5ce4b7b753deb840ed6534db84baca1
iamvigneshwars <iamvigneshwars@gmail.com>
2025-01-23 12:58:26 +0000

    feat(frontend): add grafana link to dashboard

--- frontend/dashboard/src/components/dashboard.tsx
+++ frontend/dashboard/src/components/dashboard.tsx
@@ -1,12 +1,13 @@
-import { Container, 
-         Grid2 as Grid,
-         Card,
-         CardContent,
-         Typography,
-         CardActionArea
-} from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import {
+  Container,
+  Grid2 as Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardActionArea,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const workflowsCard = (
   <React.Fragment>
@@ -15,84 +16,116 @@ const workflowsCard = (
         <Typography gutterBottom variant="h5" component="div">
           Workflows
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Provides an overview of current and past workflows for 
-          a given visit.
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Provides an overview of current and past workflows for a given visit.
         </Typography>
       </CardContent>
     </CardActionArea>
   </React.Fragment>
-)
+);
 
 const templatesCard = (
   <React.Fragment>
-    <CardActionArea component={Link} to='/templates'>
+    <CardActionArea component={Link} to="/templates">
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Templates
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
           Provides an overview of all available workflowTemplates and Sensors.
         </Typography>
       </CardContent>
     </CardActionArea>
   </React.Fragment>
-)
+);
 
 const documentationCard = (
   <React.Fragment>
-    <CardActionArea href='https://diamondlightsource.github.io/workflows/docs' target='_blank'>
+    <CardActionArea
+      href="https://diamondlightsource.github.io/workflows/docs"
+      target="_blank"
+    >
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Documentation
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Full user documentation with tutorials, how-tos, explanations and references.
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Full user documentation with tutorials, how-tos, explanations and
+          references.
         </Typography>
       </CardContent>
     </CardActionArea>
   </React.Fragment>
-)
+);
 
 const argoCard = (
   <React.Fragment>
-    <CardActionArea href='https:///argo-workflows.workflows.diamond.ac.uk' target='_blank'>
+    <CardActionArea
+      href="https:///argo-workflows.workflows.diamond.ac.uk"
+      target="_blank"
+    >
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Argo Web UI
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          The native Argo Workflows dashboard. 
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          The native Argo Workflows dashboard.
         </Typography>
       </CardContent>
     </CardActionArea>
   </React.Fragment>
-)
+);
+
+const grafanaCard = (
+  <React.Fragment>
+    <CardActionArea
+      href="https://grafana.workflows.diamond.ac.uk"
+      target="_blank"
+    >
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Grafana
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          Provides visualization for workflows metrics.
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </React.Fragment>
+);
 
 function Dashboard() {
   return (
     <>
-    <Container maxWidth="sm">
-      <Typography variant='h3' align='center' gutterBottom>
-        Workflows at Diamond
-      </Typography>
-      <Grid container alignItems="stretch" spacing={{ xs: 2, sm: 2, md: 3}} columns={{ xs: 2, sm: 4, md: 6 }}>
-          <Grid key={"workflows"} size={{ xs:2, sm: 2, md: 3 }}>
-            <Card style={{height: "100%"}}>{workflowsCard}</Card>
+      <Container maxWidth="sm">
+        <Typography variant="h3" align="center" gutterBottom>
+          Workflows at Diamond
+        </Typography>
+        <Grid
+          container
+          alignItems="stretch"
+          spacing={{ xs: 2, sm: 2, md: 3 }}
+          columns={{ xs: 2, sm: 4, md: 6 }}
+        >
+          <Grid key={"workflows"} size={{ xs: 2, sm: 2, md: 3 }}>
+            <Card style={{ height: "100%" }}>{workflowsCard}</Card>
           </Grid>
           <Grid key={"templates"} size={{ xs: 2, sm: 2, md: 3 }}>
-            <Card style={{height: "100%"}}>{templatesCard}</Card>
+            <Card style={{ height: "100%" }}>{templatesCard}</Card>
           </Grid>
           <Grid key={"documentation"} size={{ xs: 2, sm: 2, md: 3 }}>
-            <Card style={{height: "100%"}}>{documentationCard}</Card>
+            <Card style={{ height: "100%" }}>{documentationCard}</Card>
           </Grid>
-          <Grid key={"argo"} size={{ xs: 2, sm: 2, md: 3 }} >
-            <Card style={{height: "100%"}}>{argoCard}</Card>
+          <Grid key={"argo"} size={{ xs: 2, sm: 2, md: 3 }}>
+            <Card style={{ height: "100%" }}>{argoCard}</Card>
           </Grid>
-      </Grid>
-    </Container>
+          <Grid key={"grafana"} size={{ xs: 2, sm: 2, md: 3 }}>
+            <Card style={{ height: "100%" }}>{grafanaCard}</Card>
+          </Grid>
+        </Grid>
+      </Container>
     </>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
