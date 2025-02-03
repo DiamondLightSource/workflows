708a2924dddcf1f2826c1068421e3b1ee4fa465a
Yousef Moazzam <yousef.moazzam@diamond.ac.uk>
2025-02-03 12:03:18 +0000

    Add MIME type field to artifact struct

--- graph-proxy/src/graphql/workflows.rs
+++ graph-proxy/src/graphql/workflows.rs
@@ -294,19 +294,7 @@ struct Artifact<'a> {
 impl Artifact<'_> {
     /// The file name of the artifact
     async fn name(&self) -> Result<&str, WorkflowParsingError> {
-        let path = Path::new(
-            self.manifest
-                .s3
-                .as_ref()
-                .ok_or(WorkflowParsingError::UnrecognisedArtifactStore)?
-                .key
-                .as_ref()
-                .ok_or(WorkflowParsingError::MissingArtifactKey)?,
-        );
-        path.file_name()
-            .unwrap_or_default()
-            .to_str()
-            .ok_or(WorkflowParsingError::InvalidArtifactFilename)
+        artifact_filename(self.manifest)
     }
 
     /// The download URL for the artifact
@@ -322,6 +310,32 @@ impl Artifact<'_> {
         ]);
         url
     }
+
+    /// The MIME type of the artifact data
+    async fn mime_type(&self) -> Result<&str, WorkflowParsingError> {
+        let filename = artifact_filename(self.manifest)?;
+        Ok(mime_guess::from_path(filename)
+            .first_raw()
+            .unwrap_or("application/octet-stream"))
+    }
+}
+
+fn artifact_filename(
+    manifest: &IoArgoprojWorkflowV1alpha1Artifact,
+) -> Result<&str, WorkflowParsingError> {
+    let path = Path::new(
+        manifest
+            .s3
+            .as_ref()
+            .ok_or(WorkflowParsingError::UnrecognisedArtifactStore)?
+            .key
+            .as_ref()
+            .ok_or(WorkflowParsingError::MissingArtifactKey)?,
+    );
+    path.file_name()
+        .unwrap_or_default()
+        .to_str()
+        .ok_or(WorkflowParsingError::InvalidArtifactFilename)
 }
 
 /// A Task created by a workflow
@@ -1196,6 +1210,72 @@ mod tests {
                         "artifacts": [{
                             "name": "main.log",
                             "url": expected_download_url
+                        }]
+                    }]
+                }
+            }
+        });
+        assert_eq!(resp.data.into_json().unwrap(), expected_data);
+    }
+
+    #[tokio::test]
+    async fn get_artifacts_mime_type_query() {
+        let workflow_name = "numpy-benchmark-wdkwj";
+        let visit = Visit {
+            proposal_code: "mg".to_string(),
+            proposal_number: 36964,
+            number: 1,
+        };
+
+        let mut server = mockito::Server::new_async().await;
+        let mut response_file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        response_file_path.push("test-assets");
+        response_file_path.push("get-workflow-wdkwj.json");
+        let workflow_endpoint = server
+            .mock(
+                "GET",
+                &format!("/api/v1/workflows/{}/{}", visit, workflow_name)[..],
+            )
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body_from_file(response_file_path)
+            .create_async()
+            .await;
+
+        let argo_server_url = Url::parse(&server.url()).unwrap();
+        let schema = root_schema_builder()
+            .data(ArgoServerUrl(argo_server_url))
+            .data(None::<Authorization<Bearer>>)
+            .finish();
+        let query = format!(
+            r#"
+            query {{
+                workflow(name: "{}", visit: {{proposalCode: "{}", proposalNumber: {}, number: {}}}) {{
+                    status {{
+                        ...on WorkflowSucceededStatus {{
+                            tasks {{
+                                artifacts {{
+                                    name
+                                    mimeType
+                                }}
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        "#,
+            workflow_name, visit.proposal_code, visit.proposal_number, visit.number
+        );
+        let resp = schema.execute(query).await.into_result().unwrap();
+
+        workflow_endpoint.assert_async().await;
+        let expected_data = json!({
+            "workflow": {
+                "status": {
+                    "tasks": [{
+                        "artifacts": [{
+                            "name": "main.log",
+                            "mimeType": "text/plain"
                         }]
                     }]
                 }
