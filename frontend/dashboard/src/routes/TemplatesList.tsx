f2cab97dda6b1dc7fe707a4115fef71c2a203e7a
Max Herbert <max.herbert@diamond.ac.uk>
2025-05-06 11:02:20 +0100

    fix(dashboard): add error boundary around templates list

--- frontend/dashboard/src/routes/TemplatesList.tsx
+++ frontend/dashboard/src/routes/TemplatesList.tsx
@@ -2,6 +2,8 @@ import { Container, Box } from "@mui/material";
 import { ThemeProvider, DiamondTheme, Breadcrumbs } from "@diamondlightsource/sci-react-ui";
 import WorkflowsNavbar from "workflows-lib/lib/components/workflow/WorkflowsNavbar";
 import Templates from "relay-workflows-lib/lib/components/templates";
+import { Suspense } from "react";
+import WorkflowsErrorBoundary from "workflows-lib/lib/components/workflow/WorkflowsErrorBoundary";
 
 const TemplatesList: React.FC = () => {
   return (
@@ -10,9 +12,13 @@ const TemplatesList: React.FC = () => {
         <WorkflowsNavbar />
         <Breadcrumbs path={window.location.pathname} />
         <Container maxWidth="sm">
-          <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
-            <Templates />
-          </Box>
+          <WorkflowsErrorBoundary>
+            <Suspense>
+              <Box display="flex" flexDirection="column" alignItems="center" mt={2}>
+                <Templates />
+              </Box>
+            </Suspense>
+          </WorkflowsErrorBoundary>
         </Container>
       </ThemeProvider>
     </>
