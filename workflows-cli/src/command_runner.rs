d247cef1e746c6b0dbd73eb292e8e1c067f701e0
Max Herbert <max.herbert@diamond.ac.uk>
2025-05-23 14:39:39 +0100

    feat: workflows-cli

---
+++ workflows-cli/src/command_runner.rs
@@ -0,0 +1,143 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::env;
+use std::fs;
+use std::io;
+use std::os::unix::process::ExitStatusExt;
+use std::process::{Command, Output};
+
+/// Underlying traits used to run/mock CLI commands
+pub trait CommandLike {
+    /// Command arguments
+    fn arg(&mut self, arg: &str) -> &mut dyn CommandLike;
+    /// Execute command
+    fn output(&mut self) -> io::Result<Output>;
+}
+
+/// Real command runner
+pub struct RealCommand {
+    /// Command instance
+    cmd: Command,
+}
+
+/// Add arg and ouput methods to Realcommand
+impl CommandLike for RealCommand {
+    fn arg(&mut self, arg: &str) -> &mut dyn CommandLike {
+        self.cmd.arg(arg);
+        self
+    }
+
+    fn output(&mut self) -> io::Result<Output> {
+        self.cmd.output()
+    }
+}
+
+/// Shared 'new' command trait between real and mock runners
+pub trait CommandFactory {
+    /// Returns a CommandLike runner
+    fn new_command(&self, cmd: &str) -> Box<dyn CommandLike>;
+}
+
+/// Return a real command runner
+pub struct RealCommandFactory;
+
+/// Heap allocated command to assign command runner at run-time
+impl CommandFactory for RealCommandFactory {
+    fn new_command(&self, cmd: &str) -> Box<dyn CommandLike> {
+        Box::new(RealCommand {
+            cmd: Command::new(cmd),
+        })
+    }
+}
+
+/// Structure of mock entry as in mapping file
+#[derive(Debug, Deserialize, Default, Clone)]
+struct MockEntry {
+    /// Calling command
+    command: String,
+    /// CLI return
+    response: String,
+    /// Exit code
+    code: i32,
+}
+
+/// Required fields to mock real command
+pub struct MockCommand {
+    /// Command being used
+    cmd: String,
+    /// CLI arguents
+    args: Vec<String>,
+    /// Map CLI call to output
+    mappings: Vec<MockEntry>,
+}
+
+impl MockCommand {
+    /// Initiate mock command runner
+    pub fn new(cmd: &str) -> Self {
+        let yaml_str = fs::read_to_string("./tests/mock_commands.yaml")
+            .expect("Failed to read mock_commands.yaml");
+
+        let mappings: HashMap<String, Vec<MockEntry>> =
+            serde_yaml::from_str(&yaml_str).expect("Failed to parse YAML");
+
+        let target_mapping = env::var("RUST_TEST_ACTIVE_MAPPING").unwrap();
+        let active_mapping = mappings.get(&target_mapping).unwrap();
+
+        Self {
+            cmd: cmd.to_string(),
+            args: vec![],
+            mappings: active_mapping.clone(),
+        }
+    }
+
+    /// Lookup command in the mapping table
+    fn get_response(&self) -> (String, i32) {
+        let full_command = format!("{} {}", self.cmd, self.args.join(" "));
+        let entry = self
+            .mappings
+            .iter()
+            .find(|entry| entry.command == full_command)
+            .unwrap();
+        (entry.response.clone(), entry.code)
+    }
+}
+
+impl CommandLike for MockCommand {
+    fn arg(&mut self, arg: &str) -> &mut dyn CommandLike {
+        self.args.push(arg.to_string());
+        self
+    }
+
+    fn output(&mut self) -> io::Result<Output> {
+        let response = self.get_response();
+        println!(
+            "[MockCommand] Simulated command: {} {}",
+            self.cmd,
+            self.args.join(" ")
+        );
+
+        Ok(Output {
+            status: std::process::ExitStatus::from_raw(response.1),
+            stdout: response.0.as_bytes().to_vec(),
+            stderr: vec![],
+        })
+    }
+}
+
+/// Mock factory used to run argo/helm in tests
+pub struct MockCommandFactory;
+
+/// New command trait fo the mock factory
+impl CommandFactory for MockCommandFactory {
+    fn new_command(&self, cmd: &str) -> Box<dyn CommandLike> {
+        Box::new(MockCommand::new(cmd))
+    }
+}
+
+/// Get an instance of the CLI builder
+pub fn get_command_factory() -> Box<dyn CommandFactory> {
+    match env::var("MOCK_COMMAND") {
+        Ok(val) if val == "1" => Box::new(MockCommandFactory),
+        _ => Box::new(RealCommandFactory),
+    }
+}
---
+++ workflows-cli/src/main.rs
@@ -0,0 +1,88 @@
+#![warn(missing_docs)]
+#![allow(clippy::missing_docs_in_private_items)]
+//! CLI for working with Workflows at Diamond
+
+/// Argo workflows linter wrapper
+mod linter;
+use std::path::PathBuf;
+
+/// Wraps command to allow mocking
+mod command_runner;
+
+use clap::Parser;
+use std::process::Command;
+
+use crate::linter::{ManifestType, config_lint};
+
+/// Workflows Tool
+#[derive(Debug, Parser)]
+#[allow(clippy::large_enum_variant)]
+enum Cli {
+    /// Lint the templates
+    Lint(LintArgs),
+    ///Lint from a config file
+    LintConfig(LintConfigArgs),
+}
+
+/// Arguments for linting from a configfile
+#[derive(Debug, Parser)]
+struct LintConfigArgs {
+    /// Location of the workflows-lint config file. Used when running as an action
+    config_file: PathBuf,
+}
+
+/// Arguments for linting
+#[derive(Debug, Parser)]
+struct LintArgs {
+    /// Target either conventionaly WorkflowTemplates, or helm based templates
+    #[arg(long, default_value = "manifest")]
+    manifest_type: ManifestType,
+
+    /// Lint all file in folder or chart.
+    #[arg(short, long)]
+    all: bool,
+
+    /// Path to manifest(s). Expects a file, or directory if --all
+    file_name: PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if let Err(err) = ensure_cli() {
+        eprintln!("Error: {}", err);
+        std::process::exit(1)
+    }
+
+    match args {
+        Cli::Lint(args) => {
+            linter::lint(args);
+        }
+        Cli::LintConfig(args) => {
+            config_lint(args);
+        }
+    }
+}
+
+/// Checks the underlying argo and helm CLI are present
+fn ensure_cli() -> Result<(), String> {
+    let helm_output = Command::new("which")
+        .arg("helm")
+        .output()
+        .map_err(|_| "Failed to run 'which helm'. Is it installed?")?;
+
+    if !helm_output.status.success() {
+        return Err("Could not find 'helm' on PATH. Is it installed?".into());
+    }
+
+    let argo_output = Command::new("which")
+        .arg("argo")
+        .output()
+        .map_err(|_| "Failed to run 'which argo'. Is it installed?")?;
+
+    if !argo_output.status.success() {
+        return Err("Could not find 'argo' on PATH. Is it installed?".into());
+    }
+
+    Ok(())
+}
