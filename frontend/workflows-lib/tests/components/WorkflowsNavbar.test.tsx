50b03d68564c26310ce96da4da7aab4328f02aa4
Max Herbert <max.herbert@diamond.ac.uk>
2025-06-02 07:45:15 +0000

    test: update navbar tests

--- frontend/workflows-lib/tests/components/WorkflowsNavbar.test.tsx
+++ frontend/workflows-lib/tests/components/WorkflowsNavbar.test.tsx
@@ -1,20 +1,26 @@
 import { render } from '@testing-library/react';
-
 import "@testing-library/jest-dom";
-
+import { ThemeProvider } from '@mui/material/styles';
 import { DiamondTheme } from "@diamondlightsource/sci-react-ui";
 import WorkflowsNavbar from '../../lib/components/workflow/WorkflowsNavbar';
 
 describe('WorkflowsNavbar', () => {
   it('renders with title and sessionInfo', () => {
-    const { getByText } = render(<WorkflowsNavbar sessionInfo="cm12345-6" />);
+    const { getByText } = render(
+      <ThemeProvider theme={DiamondTheme}>
+        <WorkflowsNavbar sessionInfo="cm12345-6" />
+      </ThemeProvider>
+    );
     expect(getByText('cm12345-6')).toBeInTheDocument();
   });
 
   it('applies the correct styles', () => {
-    const { getByText } = render(<WorkflowsNavbar sessionInfo="cm34567-8" />);
+    const { getByText } = render(
+      <ThemeProvider theme={DiamondTheme}>
+        <WorkflowsNavbar sessionInfo="cm34567-8" />
+      </ThemeProvider>
+    );
     const sessionElement = getByText('cm34567-8');
-
     expect(sessionElement).toHaveStyle(`color: ${DiamondTheme.palette.primary.contrastText}`);
   });
 });
