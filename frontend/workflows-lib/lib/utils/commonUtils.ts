7977576b4d8b287673ac845ae1508d3d5b7c119a
VictoriaBeilstenEdmands <victoria.beilsten.edmands@gmail.com>
2025-06-12 12:56:46 +0100

    fix(frontend): use modified visitRegex in template regex

--- frontend/workflows-lib/lib/utils/commonUtils.ts
+++ frontend/workflows-lib/lib/utils/commonUtils.ts
@@ -1,5 +1,6 @@
-import { Visit, regexToVisit, visitRegex } from "@diamondlightsource/sci-react-ui";
+import { Visit, regexToVisit } from "@diamondlightsource/sci-react-ui";
 
+const visitRegex = /^([a-z]{2})([1-9]\d*)-([1-9]\d*)/;
 export const visitWithTemplateRegex = new RegExp(`${visitRegex.source}-(.+)$`);
 
 export function visitTextToVisit(visitid?: string): Visit | null {
