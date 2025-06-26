637694f49a96f489ba6077f3cc46688ef6c03552
root <jacob.williamson@diamond.ac.uk>
2025-06-26 10:19:43 +0000

    Improve formatting

--- frontend/workflows-lib/lib/components/workflow/ImageGallery.tsx
+++ frontend/workflows-lib/lib/components/workflow/ImageGallery.tsx
@@ -11,15 +11,12 @@ export const ImageGallery: React.FC<ImageGalleryProps> = ({ artifactList }) => {
   const imageArtifactsInfos: ImageInfo[] = useMemo(() => {
     return artifactList.filter((artifact) => artifact.mimeType === "image/png")
     .map((artifact, index) => ({
-        src: artifact.url,
-        alt: artifact.name || `Gallery Image ${String(index + 1)}`,
-      }));
+      src: artifact.url,
+      alt: `Gallery Image ${String(index + 1)}`,
+    }));
   }, [artifactList]);
 
-
   return (
-    <>
-      { <ScrollableImages images={imageArtifactsInfos} backgroundColor="#FFF"/> }
-    </>
+    <ScrollableImages images={imageArtifactsInfos} backgroundColor="#FFF"/>
   );
 };
