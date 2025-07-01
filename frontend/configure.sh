a1bfb9c902d2cb70472df51db41bb0b6e83395f2
Max Herbert <max.herbert@diamond.ac.uk>
2025-07-01 15:02:55 +0100

    feat(dashboard): determine config at runtime

---
+++ frontend/configure.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+if [ -z "$SOURCE_DIR" ]; then
+    echo "Source directory of HTML is not set"
+    exit 1
+fi
+
+replace_placeholder() {
+    local var_name="$1"
+    local value=$(eval echo "\$$var_name")
+
+    if [ -z "$value" ]; then
+        echo "Environment variable for '$var_name' is not set"
+        exit 1
+    fi
+
+    local placeholder="{{ $var_name }}"
+    local sed_cmd="s|$placeholder|$value|g"
+
+    grep -rl "$placeholder" "$SOURCE_DIR" | xargs -r sed -i "$sed_cmd"
+}
+
+
+replace_placeholder KEYCLOAK_URL
+replace_placeholder KEYCLOAK_REALM
+replace_placeholder KEYCLOAK_CLIENT
+
+nginx -g 'daemon off;'
