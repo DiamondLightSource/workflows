45d72ed620c6b175bc12067cd056bdbf270a6919
VictoriaBeilstenEdmands <victoria.beilsten.edmands@gmail.com>
2025-07-03 10:02:24 +0100

    fix(frontend): fix Accordion story with MemoryRouter

--- frontend/workflows-lib/stories/WorkflowAccordion.stories.tsx
+++ frontend/workflows-lib/stories/WorkflowAccordion.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { MemoryRouter } from "react-router-dom";
 import { fakeWorkflowA } from "./common";
 import WorkflowAccordion from "../lib/components/workflow/WorkflowAccordion";
 import TasksFlow from "../lib/components/workflow/TasksFlow";
@@ -7,6 +8,14 @@ import { fakeTasksA } from "./common";
 const meta: Meta<typeof WorkflowAccordion> = {
   title: "Workflow",
   component: WorkflowAccordion,
+
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={["/"]}>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
 };
 
 type Story = StoryObj<typeof WorkflowAccordion>;
