23b70a087b7753ff823e252f74b9cbd715af73e5
Thomas Binu Thomas <thomas.thomas@diamond.ac.uk>
2025-07-23 16:01:21 +0000

    feat(frontend): add table view of the artefacts shown

--- frontend/workflows-lib/lib/components/workflow/ArtifactFilteredList.tsx
+++ frontend/workflows-lib/lib/components/workflow/ArtifactFilteredList.tsx
@@ -1,13 +1,15 @@
 import React, { useState, useMemo } from "react";
 import {
-  Box,
-  Typography,
-  List,
-  ListItemButton,
-  Tooltip,
   ToggleButtonGroup,
   ToggleButton,
-  ListSubheader,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Typography,
+  TableContainer,
+  Paper,
 } from "@mui/material";
 import type { Artifact } from "workflows-lib";
 
@@ -50,72 +52,86 @@ export const ArtifactFilteredList: React.FC<ArtifactFilteredListProps> = ({
   }, [artifactFilter, artifactList, imageArtifacts]);
   return (
     <>
-      <Box
+      <Typography variant="h5" sx={{ marginTop: 2, marginBottom: 2 }}>
+        Artefacts
+      </Typography>
+      <ToggleButtonGroup
+        value={artifactFilter}
+        exclusive
+        onChange={handleArtifactFilter}
+        aria-label="artifact filter"
+      >
+        <ToggleButton value="all" aria-label="all">
+          ALL
+        </ToggleButton>
+        <ToggleButton value="log" aria-label="log">
+          LOG
+        </ToggleButton>
+        <ToggleButton value="text" aria-label="text">
+          TEXT
+        </ToggleButton>
+        <ToggleButton value="images" aria-label="images">
+          IMAGES
+        </ToggleButton>
+      </ToggleButtonGroup>
+      <TableContainer
+        component={Paper}
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "flex-start",
-          p: 2,
+          borderRadius: "8px",
+          border: "1px solid #ccc",
+          marginTop: 2,
         }}
       >
-        <ToggleButtonGroup
-          value={artifactFilter}
-          exclusive
-          onChange={handleArtifactFilter}
-          aria-label="artifact filter"
-        >
-          <ToggleButton value="all" aria-label="all">
-            ALL
-          </ToggleButton>
-          <ToggleButton value="log" aria-label="log">
-            LOG
-          </ToggleButton>
-          <ToggleButton value="text" aria-label="text">
-            TEXT
-          </ToggleButton>
-          <ToggleButton value="images" aria-label="images">
-            IMAGES
-          </ToggleButton>
-        </ToggleButtonGroup>
-        <List
-          sx={{ paddingRight: "200px", width: "450px", paddingTop: 0 }}
-          subheader={<ListSubheader>Artifacts</ListSubheader>}
-        >
-          {listedArtifacts.map((artifact) => (
-            <Tooltip title={artifact.name} enterDelay={400} key={`${artifact.parentTask}-${artifact.name}`}>
-              <ListItemButton
-                onClick={() => window.open(artifact.url, "_blank")}
+        <Table sx={{ width: "100%" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{
+                  borderRight: "1px solid #ccc",
+                }}
               >
-                <Box display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" width="100%">
-                  <Typography
-                    noWrap
-                    sx={{
-                      textOverflow: "ellipsis",
-                      overflow: "hidden",
-                      whiteSpace: "nowrap",
-                    }}
-                  >
-                    {artifact.name}
-                  </Typography>
-                  {artifact.parentTask && (
-                    <Typography
-                      noWrap
-                      color="#777"
-                      sx={{
-                        textOverflow: "ellipsis",
-                        overflow: "hidden",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {artifact.parentTask}
-                    </Typography>
-                  )}
-                </Box>
-              </ListItemButton>
-            </Tooltip>
-          ))}
-        </List>
-      </Box>
+                <Typography variant="h6">Artefact Name</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography variant="h6">Parent Task</Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listedArtifacts.map((artifact) => (
+              <TableRow
+                key={`${artifact.parentTask}-${artifact.name}`}
+                hover
+                onClick={() => window.open(artifact.url, "_blank")}
+                style={{ cursor: "pointer" }}
+              >
+                <TableCell
+                  sx={{
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    maxWidth: "300px",
+                    borderRight: "1px solid #ccc",
+                  }}
+                >
+                  {artifact.name}
+                </TableCell>
+                <TableCell
+                  sx={{
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                    whiteSpace: "nowrap",
+                    maxWidth: "200px",
+                    color: "#777",
+                  }}
+                >
+                  {artifact.parentTask || "N/A"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
