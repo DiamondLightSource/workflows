65ce8391b6d03ee422e9ee3bb1e6078c64b4ceb6
VictoriaBeilstenEdmands <victoria.beilsten.edmands@gmail.com>
2025-07-31 16:06:01 +0100

    feat(frontend): use accordion for artifact list

--- frontend/workflows-lib/lib/components/workflow/TaskInfo.tsx
+++ frontend/workflows-lib/lib/components/workflow/TaskInfo.tsx
@@ -1,40 +1,71 @@
-import React from "react";
-import { Box } from "@mui/material";
-import type { Artifact } from "workflows-lib";
-import { ImageGallery } from "./ImageGallery";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Box,
+} from "@mui/material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import { ArtifactFilteredList } from "./ArtifactFilteredList";
+import type { Artifact } from "workflows-lib";
+import { ImageInfo, ScrollableImages } from "./ScrollableImages";
+import { useMemo } from "react";
 
 interface TaskInfoProps {
   artifactList: Artifact[];
 }
 
 export const TaskInfo: React.FC<TaskInfoProps> = ({ artifactList }) => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: {
-          xs: "column",
-          sm: "row",
-        },
-        flexWrap: "wrap",
-        gap: 2,
-        width: "100%",
-        boxSizing: "border-box",
-      }}
-    >
-      <Box sx={{ flex: 1, minWidth: "300px" }}>
-        <ArtifactFilteredList artifactList={artifactList} />
-      </Box>
+  const imageArtifactsInfos: ImageInfo[] = useMemo(() => {
+    return artifactList
+      .filter((artifact) => artifact.mimeType === "image/png")
+      .map((artifact, index) => ({
+        src: artifact.url,
+        alt: `Gallery Image ${String(index + 1)}`,
+      }));
+  }, [artifactList]);
 
-      <Box
-        sx={{
-          flex: 1,
-          minWidth: "300px",
-        }}
-      >
-        <ImageGallery artifactList={artifactList} />
-      </Box>
-    </Box>
+  return (
+    <Accordion sx={{ width: "100%", my: 0 }} defaultExpanded>
+      <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+        <Typography variant="h6">Output Information</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              sm: "row",
+            },
+            flexWrap: "wrap",
+            gap: 2,
+            width: "100%",
+            boxSizing: "border-box",
+          }}
+        >
+          <Box sx={{ flex: 1, minWidth: "300px" }}>
+            <ArtifactFilteredList artifactList={artifactList} />
+          </Box>
+
+          <Box
+            sx={{
+              flex: 1,
+              minWidth: "300px",
+              visibility: imageArtifactsInfos.length > 0 ? "visible" : "hidden",
+              mt: 8,
+              paddingLeft: 7,
+            }}
+          >
+            {imageArtifactsInfos.length > 0 && (
+              <ScrollableImages
+                images={imageArtifactsInfos}
+                backgroundColor="#FFF"
+              />
+            )}
+          </Box>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
   );
 };
--- frontend/workflows-lib/tests/components/data.ts
+++ frontend/workflows-lib/tests/components/data.ts
@@ -1,3 +1,4 @@
+import { ImageInfo } from "../../lib/components/workflow/ScrollableImages";
 import { Artifact, TaskStatus } from "../../lib/types";
 import { Visit } from "@diamondlightsource/sci-react-ui";
 
@@ -5,7 +6,7 @@ export const logArtifact: Artifact = {
   name: "main.log",
   url: "fakepath/to/main.log",
   mimeType: "text/plain",
-  parentTask: "task"
+  parentTask: "task",
 };
 
 export const mockArtifacts: Artifact[] = [
@@ -14,20 +15,36 @@ export const mockArtifacts: Artifact[] = [
     name: "image1.png",
     mimeType: "image/png",
     url: "fakepath/to/image1.png",
-    parentTask: "task1"
+    parentTask: "task1",
   },
 
   {
     name: "textfile.txt",
     mimeType: "text/plain",
     url: "fakepath/to/textfile.txt",
-    parentTask: "task2"
+    parentTask: "task2",
   },
   {
     name: "image2.png",
     mimeType: "image/png",
     url: "fakepath/to/image2.png",
-    parentTask: "task3"
+    parentTask: "task3",
+  },
+];
+
+export const mockImages: ImageInfo[] = [
+  {
+    src: "fakepath/to/image1.png",
+    alt: "Gallery Image 1",
+  },
+
+  {
+    src: "fakepath/to/image2.png",
+    alt: "Gallery Image 2",
+  },
+  {
+    src: "fakepath/to/image3.png",
+    alt: "Gallery Image 3",
   },
 ];
 
@@ -45,7 +62,7 @@ export const mockTasks = [
     artifacts: [logArtifact],
     workflow: "workflow-test",
     instrumentSession: instrumentSession,
-    stepType: "POD"
+    stepType: "POD",
   },
   {
     id: "task-2",
@@ -55,7 +72,7 @@ export const mockTasks = [
     workflow: "workflow-test",
     instrumentSession: instrumentSession,
     depends: ["task-1"],
-    stepType: "POD"
+    stepType: "POD",
   },
   {
     id: "task-3",
@@ -64,6 +81,6 @@ export const mockTasks = [
     artifacts: [logArtifact],
     workflow: "workflow-test",
     instrumentSession: instrumentSession,
-    stepType: "POD"
+    stepType: "POD",
   },
 ];
