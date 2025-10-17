a9e423ca01bfe5ed822213e92ab4ef1373002b11
James Gilbert <james.gilbert@diamond.ac.uk>
2025-10-17 09:29:53 +0000

    test(frontend): use graphql mocks in tests
    test(frontend): mock out template cards
    
    test(frontend): compile relay in CI tests

--- frontend/relay-workflows-lib/vite.config.ts
+++ frontend/relay-workflows-lib/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import relay from "vite-plugin-relay";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [relay, react()],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
