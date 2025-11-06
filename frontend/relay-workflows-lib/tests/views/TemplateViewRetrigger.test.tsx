dce0efbb758ab70036539ea663dd2ec07aadc81c
James Gilbert <james.gilbert@diamond.ac.uk>
2025-11-06 15:09:39 +0000

    test(frontend): add tests for TemplateViewRetrigger

---
+++ frontend/relay-workflows-lib/tests/views/TemplateViewRetrigger.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { server } from "relay-workflows-lib/tests/mocks/browser.ts";
+import { RelayEnvironmentProvider } from "react-relay";
+import { getRelayEnvironment } from "dashboard/src/RelayEnvironment";
+import { MemoryRouter } from "react-router-dom";
+import TemplateViewWithRetrigger from "../../lib/views/TemplateViewRetrigger";
+
+describe("TemplateViewRetrigger", () => {
+  beforeAll(() => {
+    server.listen();
+  });
+
+  beforeEach(async () => {
+    const environment = await getRelayEnvironment();
+
+    render(
+      <MemoryRouter>
+        <RelayEnvironmentProvider environment={environment}>
+          <TemplateViewWithRetrigger
+            templateName="e02-mib2x"
+            workflowName="e02-mib2x"
+            visit={{ proposalCode: "mg", proposalNumber: 36964, number: 1 }}
+          />
+        </RelayEnvironmentProvider>
+      </MemoryRouter>,
+    );
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("passes parameter fragment data to the submission form correctly", async () => {
+    expect(await screen.findByText("ePSIC mib conversion")).toBeInTheDocument();
+    expect(screen.getByRole("textbox", { name: "Memory" })).toHaveValue("8Gi");
+    expect(
+      screen.getByRole("spinbutton", { name: "Dimension in x (row)" }),
+    ).toHaveValue(128);
+  });
+});
