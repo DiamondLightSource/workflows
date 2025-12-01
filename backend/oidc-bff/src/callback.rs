5973b00770a1dbea45c66512f19f427b0ad5ecec
David Hadley <davehadley@users.noreply.github.com>
2025-12-01 14:26:38 +0000

    fix url

--- backend/oidc-bff/src/callback.rs
+++ backend/oidc-bff/src/callback.rs
@@ -42,9 +42,9 @@ pub async fn callback(
     if auth_session_data.csrf_token != CsrfToken::new(params.state) {
         return Err(anyhow!("invalid state").into());
     }
-
+    let url_prefix = "https://staging.workflows.diamond.ac.uk"; // TODO: this should come from config
     let redirect_url = Cow::Owned(RedirectUrl::new(
-        "http://localhost:5173/auth/callback".to_string(),
+        format!("{}/auth/callback", url_prefix).to_string(),
     )?);
     // Now you can exchange it for an access token and ID token.
     let token_response = state
