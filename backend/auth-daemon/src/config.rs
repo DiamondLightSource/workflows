ffcaf955ccbc36de2face819615cc8c5dd010e9d
David Hadley <davehadley@users.noreply.github.com>
2025-12-19 10:17:23 +0000

    Proof-of-concept authentication in workflows with Authoirization Code Flow with PKCE method

---
+++ backend/auth-daemon/src/config.rs
@@ -0,0 +1,32 @@
+use std::path::Path;
+
+use serde::{Deserialize, Serialize};
+use crate::Result;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub client_id: String,
+    pub client_secret: String,
+    pub oidc_provider_url: String,
+    pub graph_url: String,
+    pub port: u16,
+    pub postgres_user: String,
+    pub postgres_password: String,
+    pub postgres_database: String,
+    pub postgres_hostname: String,
+    pub postgres_port: u16,
+    pub encryption_public_key: String,
+    pub encryption_private_key: String,
+}
+
+impl Config {
+    /// Load config from JSON or YAML file
+    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let content = std::fs::read_to_string(&path)?;
+        match path.as_ref().extension().and_then(|e| e.to_str()) {
+            Some("json") => Ok(serde_json::from_str(&content)?),
+            // otherwise assume yaml
+            _ => Ok(serde_yaml::from_str(&content)?),
+        }
+    }
+}
---
+++ backend/oidc-bff/src/login.rs
@@ -0,0 +1,49 @@
+use std::sync::Arc;
+
+use axum::extract::State;
+use axum::response::Redirect;
+use openidconnect::core::{CoreAuthenticationFlow, CoreClient, CoreProviderMetadata};
+use openidconnect::reqwest;
+use openidconnect::{
+    ClientId, ClientSecret, CsrfToken, IssuerUrl, Nonce, PkceCodeChallenge, RedirectUrl, Scope,
+};
+use tower_sessions::Session;
+
+use crate::Result;
+use crate::auth_session_data::LoginSessionData;
+use crate::state::AppState;
+
+#[axum::debug_handler]
+pub async fn login(State(state): State<Arc<AppState>>, session: Session) -> Result<Redirect> {
+    // Set the URL the user will be redirected to after the authorization process.
+    // .set_redirect_uri(RedirectUrl::new("https://localhost/callback".to_string())?);
+    let oidc_client = state.oidc_client.clone().set_redirect_uri(RedirectUrl::new(
+        // "http://localhost:5173/auth/callback".to_string(),
+        "https://staging.workflows.diamond.ac.uk/auth/callback".to_string(),
+    )?);
+    // .set_redirect_uri(RedirectUrl::new("https://workflows.diamond.ac.uk".to_string())?)
+    // Generate a PKCE challenge.
+    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    // Generate the full authorization URL.
+    let (auth_url, csrf_token, nonce) = oidc_client
+        .authorize_url(
+            CoreAuthenticationFlow::AuthorizationCode,
+            CsrfToken::new_random,
+            Nonce::new_random,
+        )
+        // Set the desired scopes.
+        .add_scope(Scope::new("openid".to_string()))
+        .add_scope(Scope::new("offline_access".to_string()))
+        // Set the PKCE code challenge.
+        .set_pkce_challenge(pkce_challenge)
+        .url();
+
+    // Store data in the users session
+    let auth_session_data = LoginSessionData::new(csrf_token, pkce_verifier, nonce);
+    session
+        .insert(LoginSessionData::SESSION_KEY, auth_session_data)
+        .await?;
+
+    Ok(Redirect::temporary(auth_url.as_str()))
+}
