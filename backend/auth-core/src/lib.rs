b61a52133d9f3301b1a621e5489caeab61837a29
TBThomas56 <thomas.thomas@diamond.ac.uk>
2026-03-30 13:14:41 +0100

    feat(backend): common database module

--- backend/auth-core/src/lib.rs
+++ backend/auth-core/src/lib.rs
@@ -1,8 +1,8 @@
+pub mod config;
+pub mod database;
 pub mod entity;
 pub mod error;
 pub mod healthcheck;
-pub mod config;
 pub mod oidc;
 
 pub type Result<T> = std::result::Result<T, error::Error>;
-
