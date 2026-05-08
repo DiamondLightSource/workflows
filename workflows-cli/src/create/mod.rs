77ff9db3a0939898c41537b0864390c15795262a
Hazem Nureldin <gmm14360@cs05r-sc-serv-07.diamond.ac.uk>
2026-05-08 09:17:01 +0100

    fix: mixed python with rust code

--- workflows-cli/src/create/mod.rs
+++ workflows-cli/src/create/mod.rs
@@ -87,17 +87,19 @@ fn prompt(message: &str) -> bool {
     let mut selection = String::new();
 
     loop {
-        selection.clear();
+	selection.clear();
         io::stdin()
             .read_line(&mut selection)
             .expect("Failed to read line");
         if selection.trim().to_lowercase().as_str() {
-            "y" => return true, 
+            "y" => return true,
             "n" => return false,
             _ => println!("Invalid input. Please enter 'y' or 'n'."),
         }
     }
 }
+
+
 
 // Recursively copy directory contents
 fn copy_directory(src: &Path, dest: &Path) -> Result<(), String> {
