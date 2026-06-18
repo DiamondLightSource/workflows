5e0104332de516a61ff55673fcfbad577eeee14d
James Gilbert <james.gilbert@diamond.ac.uk>
2026-06-18 11:38:46 +0100

    feat(events): merge stop scan messages with start messages

--- charts/events/generic-servers/stomp-server.py
+++ charts/events/generic-servers/stomp-server.py
@@ -10,6 +10,7 @@ import os
 from concurrent import futures
 from pydantic import BaseModel, ConfigDict
 from typing import Literal
+from deepmerge import conservative_merger
 
 
 class EventSourceConfig(BaseModel):
@@ -21,6 +22,7 @@ class EventSourceConfig(BaseModel):
     port: int
     destination: str
     ack: Literal["auto", "client", "client-individual"] = "auto"
+
 
 def parse_config(config: bytes) -> EventSourceConfig | None:
     try:
@@ -37,10 +39,11 @@ def _build_event(payload: str) -> generic_pb2.Event:
     return generic_pb2.Event(name="workflow-trigger", payload=payload.encode())
 
 
-class ConnectionManager():
+class ConnectionManager:
     def __init__(self):
         self.connection: stomp.Connection
         self.queue = queue.SimpleQueue()
+        self.start_messages: dict[str, dict] = {}
 
     def create_connection(self, request: generic_pb2.EventSource) -> None:
         config = parse_config(request.config)
@@ -50,7 +53,7 @@ class ConnectionManager():
             return
         logging.debug("Using config: %s", config)
         conn = stomp.Connection([(config.host, config.port)], heartbeats=(10000, 10000))
-        conn.set_listener("", _StompListener(self.queue))
+        conn.set_listener("", _StompListener(self.queue, self.start_messages))
         conn.connect(
             login=config.user,
             passcode=config.password,
@@ -60,18 +63,31 @@ class ConnectionManager():
         conn.subscribe(
             destination=config.destination, id="workflows-trigger", ack=config.ack
         )
-        self.connection  = conn
+        self.connection = conn
 
     def get_queue(self) -> queue.SimpleQueue:
-        return self.queue 
+        return self.queue
 
     def get_connection(self) -> stomp.Connection:
         return self.connection
 
 
 class _StompListener(stomp.ConnectionListener):
-    def __init__(self, q):
-        self.q = q
+    def __init__(self, q, start_messages):
+        self.q: queue.SimpleQueue = q
+        self.start_messages: dict[str, dict] = start_messages
+
+    def _add_start_message(self, message: dict):
+        uid: str | None = message.get("doc", {}).get("uid")
+        if uid:
+            self.start_messages.update({uid: message})
+
+    def _merge_stop_with_start(self, stop_message: dict):
+        uid: str | None = stop_message.get("doc", {}).get("run_start")
+        if uid:
+            start_message = self.start_messages.pop(uid, {})
+            return conservative_merger.merge(stop_message, start_message)
+        logging.error(f"Unable to find start message with uid {uid}")
 
     def on_connected(self, frame: stomp.utils.Frame) -> None:
         logging.info("Connected to STOMP broker")
@@ -86,11 +102,21 @@ class _StompListener(stomp.ConnectionListener):
         logging.debug("Message received: %s", frame.body)
         if isinstance(frame.body, str):
             messageJson = json.loads(frame.body)
-            if messageJson.get("name") == "start":
+            message_type = messageJson.get("name")
+            if message_type == "start":
+                self._add_start_message(messageJson)
                 self.q.put(frame.body)
+            elif message_type == "stop":
+                matched_message = self._merge_stop_with_start(messageJson)
+                if matched_message:
+                    self.q.put(json.dumps(matched_message))
+
+            else:
+                logging.debug(f"Unsupported message type: {message_type}")
 
     def on_heartbeat(self) -> None:
         logging.debug("Hearbeat received")
+
 
 class StompEventServicer(generic_pb2_grpc.EventingServicer):
     def StartEventSource(self, request: generic_pb2.EventSource, context):
