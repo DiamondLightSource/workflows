f80bb8c36b2041c9d63d2a52804e1b40b2b05275
James Gilbert <james.gilbert@diamond.ac.uk>
2026-07-08 15:04:04 +0000

    feat(graph-proxy): add queries to get live triggers

--- backend/graph-proxy/src/graphql/triggers.rs
+++ backend/graph-proxy/src/graphql/triggers.rs
@@ -4,10 +4,13 @@ use crate::{
     graphql::{auth_guard::AuthGuard, subscription::get_auth_token, VisitInput},
     KubernetesApiUrl,
 };
-use async_graphql::{Context, Object, SimpleObject};
+use async_graphql::{
+    connection::{Connection, CursorType, Edge, EmptyFields, OpaqueCursor},
+    Context, Object, SimpleObject,
+};
 use jsonwebtoken::dangerous::insecure_decode;
 use kube::{
-    api::{ObjectMeta, PostParams},
+    api::{ListParams, ObjectMeta, PostParams},
     Api, Client, Config, CustomResource,
 };
 use schemars::JsonSchema;
@@ -24,6 +27,12 @@ enum TriggerError {
     MissingPosixUid,
     #[error(r#"Token not found"#)]
     MissingToken,
+    #[error(r#"Unable to infer Kubernetes config"#)]
+    ConfigInferError,
+    #[error(r#"Unable to create Kubernetes config"#)]
+    ClientCreationError,
+    #[error(r#"Forbidden from accessing resource"#)]
+    ForbiddenAccess,
 }
 
 /// The contents of the `spec` field of the Trigger custom resource. Used to generate the Trigger root object
@@ -48,6 +57,8 @@ struct TriggerGQL {
     name: Option<String>,
     /// The name of a ClusterTriggerTemplate that the Trigger is created from
     template_ref: String,
+    /// The beamline that the Trigger monitors
+    beamline: Option<String>,
 }
 
 impl From<Trigger> for TriggerGQL {
@@ -55,7 +66,120 @@ impl From<Trigger> for TriggerGQL {
         Self {
             name: t.metadata.name,
             template_ref: t.spec.template_ref,
+            beamline: t
+                .metadata
+                .labels
+                .as_ref()
+                .and_then(|l| l.get("workflows.diamond.ac.uk/beamline").cloned()),
         }
+    }
+}
+
+/// Infers the Kubernetes client
+async fn setup_client(ctx: &Context<'_>) -> Result<Client, TriggerError> {
+    let kubernetes_api_url = ctx.data_unchecked::<KubernetesApiUrl>();
+    let mut config = Config::infer()
+        .await
+        .map_err(|_| TriggerError::ConfigInferError)?;
+    config.cluster_url = kubernetes_api_url.deref().clone();
+    Client::try_from(config).or(Err(TriggerError::ClientCreationError))
+}
+
+/// Decodes the JWT to obtain the user's posix uid
+async fn get_posix_from_ctx(ctx: &Context<'_>) -> Result<String, TriggerError> {
+    let auth_token = get_auth_token(ctx).map_err(|_| TriggerError::MissingToken)?;
+
+    let claims = insecure_decode::<Value>(auth_token)
+        .map_err(|_| TriggerError::TokenDecodeError)?
+        .claims;
+
+    claims["posix_uid"]
+        .as_str()
+        .map(str::to_owned)
+        .ok_or(TriggerError::MissingPosixUid)
+}
+
+/// Queries related to [`Trigger`]s
+#[derive(Debug, Clone, Default)]
+pub struct TriggerQuery;
+
+#[Object(guard = "AuthGuard")]
+impl TriggerQuery {
+    /// Get a specific Trigger by name and visit
+    async fn trigger(
+        &self,
+        ctx: &Context<'_>,
+        name: String,
+        visit: Option<String>,
+    ) -> anyhow::Result<Option<TriggerGQL>> {
+        let client = setup_client(ctx).await?;
+        let posix_uid = get_posix_from_ctx(ctx).await?;
+        let api: Api<Trigger> = Api::namespaced(client, &visit.unwrap_or("events".to_string()));
+        match api.get(&name).await {
+            Ok(trigger) => {
+                if trigger.clone().metadata.labels.is_some_and(|f| {
+                    f.get("workflows.diamond.ac.uk/posixuid")
+                        .is_some_and(|l| l == &posix_uid)
+                }) {
+                    Ok(Some(trigger.into()))
+                } else {
+                    Err(TriggerError::ForbiddenAccess.into())
+                }
+            }
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    /// Get multiple Triggers across namespaces
+    async fn triggers(
+        &self,
+        ctx: &Context<'_>,
+        cursor: Option<String>,
+        limit: Option<u32>,
+    ) -> anyhow::Result<Connection<OpaqueCursor<String>, TriggerGQL, EmptyFields, EmptyFields>>
+    {
+        let client = setup_client(ctx).await?;
+        let posix_uid = get_posix_from_ctx(ctx).await?;
+        let api: Api<Trigger> = Api::all(client);
+
+        let continue_token = cursor
+            .as_ref()
+            .map(|cursor| {
+                OpaqueCursor::<String>::decode_cursor(cursor)
+                    .map(|c| c.0)
+                    .map_err(|_| anyhow::anyhow!("Cursor not valid"))
+            })
+            .transpose()?;
+
+        let mut lp = ListParams::default()
+            .labels(format!("workflows.diamond.ac.uk/posixuid={}", posix_uid).as_str())
+            .limit(limit.unwrap_or(10));
+
+        if let Some(token) = &continue_token {
+            lp = lp.continue_token(token);
+        }
+
+        let triggers_response = api.list(&lp).await?;
+
+        let triggers = triggers_response
+            .items
+            .into_iter()
+            .map(TriggerGQL::from)
+            .collect::<Vec<_>>();
+
+        let next_continue_token = triggers_response.metadata.continue_;
+
+        let mut connection =
+            Connection::new(continue_token.is_some(), next_continue_token.is_some());
+
+        connection.edges.extend(triggers.into_iter().map(|trigger| {
+            Edge::new(
+                OpaqueCursor(next_continue_token.clone().unwrap_or_default()),
+                trigger,
+            )
+        }));
+
+        Ok(connection)
     }
 }
 
@@ -73,26 +197,17 @@ impl TriggerMutation {
         name: Option<String>,
         visit: Option<VisitInput>,
     ) -> anyhow::Result<Option<TriggerGQL>> {
-        let kubernetes_api_url = ctx.data_unchecked::<KubernetesApiUrl>();
-        let auth_token: String = get_auth_token(ctx).map_err(|_| TriggerError::MissingToken)?;
-        let claims = insecure_decode::<Value>(auth_token)
-            .map_err(|_| TriggerError::TokenDecodeError)?
-            .claims;
-        let posix_uid = claims["posix_uid"]
-            .as_str()
-            .ok_or(TriggerError::MissingPosixUid)?;
-        let mut config = Config::infer().await?;
-        config.cluster_url = kubernetes_api_url.deref().clone();
-        let client = Client::try_from(config)?;
+        let client = setup_client(ctx).await?;
+        let posix_uid = get_posix_from_ctx(ctx).await?;
         let namespace = visit.map_or(String::from("events"), |v| v.to_string());
-        let api: Api<Trigger> = Api::namespaced(client.clone(), &namespace);
+        let api: Api<Trigger> = Api::namespaced(client, &namespace);
         let trigger = Trigger {
             metadata: ObjectMeta {
                 generate_name: Some(format!("{}-", template_ref)),
                 name,
                 labels: Some(BTreeMap::from([(
                     String::from("workflows.diamond.ac.uk/posixuid"),
-                    String::from(posix_uid),
+                    posix_uid,
                 )])),
                 ..Default::default()
             },
@@ -273,4 +388,36 @@ mod tests {
         );
         trigger_mutation(query, "namespaced-trigger.json", "mg36964-1", expected).await
     }
+
+    // #[tokio::test]
+    // async fn get_single_trigger() -> anyhow::Result<()> {
+    //     let _ = rustls::crypto::ring::default_provider().install_default();
+    //     let mut server = mockito::Server::new_async().await;
+    //     let kubeconfig = format!(
+    //         r#"
+    //         apiVersion: v1
+    //         kind: Config
+    //         clusters:
+    //         - cluster:
+    //             server: {}
+    //           name: test
+    //         contexts:
+    //         - context:
+    //             cluster: test
+    //             user: test
+    //           name: test
+    //         current-context: test
+    //         users:
+    //         - name: test
+    //           user: {{}}
+    //         "#,
+    //         server.url()
+    //     );
+
+    //     let file = tempfile::NamedTempFile::new()?;
+    //     std::fs::write(file.path(), kubeconfig)?;
+    //     std::env::set_var("KUBECONFIG", file.path());
+
+    //     server.mock(method, path)
+    // }
 }
