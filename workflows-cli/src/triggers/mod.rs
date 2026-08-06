caa6180cfa9ab9d6147dca949786e339a648ecbb
James Gilbert <james.gilbert@diamond.ac.uk>
2026-08-06 09:48:32 +0000

    refactor: use auth-core [no ci]

--- workflows-cli/src/triggers/mod.rs
+++ workflows-cli/src/triggers/mod.rs
@@ -1,17 +1,21 @@
 use crate::TriggerCreateArgs;
+use auth_core::oauth2::{AccessToken, RefreshToken, TokenResponse};
+use auth_core::{
+    config::CommonConfig,
+    oidc::{create_oidc_client, exchange_refresh_token},
+};
 use gql_client::Client;
 use jsonwebtoken::dangerous::insecure_decode;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::{
     collections::HashMap,
-    fs::{self, DirEntry, File, OpenOptions},
+    fs::{self, DirEntry, File},
     path::Path,
     time::{SystemTime, UNIX_EPOCH},
 };
 
 const GRAPH_URL: &str = "https://staging.workflows.diamond.ac.uk/graphql";
-const KEYCLOAK_URL: &str = "https://identity-test.diamond.ac.uk";
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -39,7 +43,10 @@ pub async fn create_trigger(args: TriggerCreateArgs) {
     "#;
     let token = get_auth_token().await;
     let mut headers = HashMap::new();
-    headers.insert("Authorization", format!("Bearer {}", token));
+    headers.insert(
+        "Authorization",
+        format!("Bearer {}", token.secret().to_string()),
+    );
     let client = Client::new_with_headers(GRAPH_URL, headers);
     let resp = client
         .query_with_vars_unwrap::<CreateTriggerResponse, TriggerCreateArgs>(mutation, args)
@@ -50,25 +57,48 @@ pub async fn create_trigger(args: TriggerCreateArgs) {
 
 #[derive(Serialize, Deserialize)]
 struct CachedTokens {
-    id_token: String,
-    refresh_token: String,
+    id_token: AccessToken,
+    refresh_token: RefreshToken,
 }
 
-#[derive(Deserialize, Serialize)]
-struct RefreshTokenResponse {
-    access_token: String,
-    id_token: String,
-    refresh_token: String,
-    expires_in: u32,
-    refresh_expires_in: u32,
-    token_type: String,
-    #[serde(rename = "not-before-policy")]
-    not_before_policy: u32,
-    session_state: String,
-    scope: String,
+async fn refresh_using_auth_core(refresh_token: &RefreshToken, path: &Path) -> AccessToken {
+    let config = CommonConfig {
+        client_id: "workflows-cli".to_string(),
+        client_secret: "".to_string(),
+        oidc_provider_url: "https://identity-test.diamond.ac.uk/realms/dls".to_string(),
+        port: 8000,
+        postgres_user: "".to_string(),
+        postgres_password: "".to_string(),
+        postgres_database: "".to_string(),
+        postgres_hostname: "".to_string(),
+        postgres_port: 0,
+        encryption_public_key: "".to_string(),
+        cors_allow: None,
+    };
+    let (oidc_client, http_client) = create_oidc_client(&config)
+        .await
+        .expect("Client setup failed");
+    let refreshed_tokens = exchange_refresh_token(&oidc_client, &http_client, refresh_token)
+        .await
+        .expect("Failed to refresh tokens");
+
+    let new_cached_tokens = CachedTokens {
+        id_token: refreshed_tokens.access_token().to_owned(),
+        refresh_token: refreshed_tokens
+            .refresh_token()
+            .expect("No refresh token")
+            .to_owned(),
+    };
+    fs::write(
+        path,
+        serde_json::to_string_pretty(&new_cached_tokens)
+            .expect("Unable to create new token object"),
+    )
+    .expect("Couldn't write to file");
+    new_cached_tokens.id_token
 }
 
-async fn get_auth_token() -> String {
+async fn get_auth_token() -> AccessToken {
     let cache_path = Path::new("/root/.kube/cache/workflows/oidc-login"); // CHANGE TO /home/user
     let dir_contents = fs::read_dir(cache_path).expect("Unable to access cached tokens directory");
     let mut newest_file: Option<DirEntry> = None;
@@ -92,7 +122,7 @@ async fn get_auth_token() -> String {
     let tokens = serde_json::from_reader::<File, CachedTokens>(file)
         .expect("Error: cached tokens are formatted incorrectly");
 
-    let expiry = insecure_decode::<Value>(&tokens.id_token)
+    let expiry = insecure_decode::<Value>(&tokens.id_token.secret().to_string())
         .expect("Unable to decode access token")
         .claims["exp"]
         .as_u64()
@@ -108,36 +138,5 @@ async fn get_auth_token() -> String {
         return tokens.id_token;
     };
 
-    get_refreshed_token(tokens.refresh_token, &file_path).await
-}
-
-async fn get_refreshed_token(refresh_token: String, path: &Path) -> String {
-    let token_url = KEYCLOAK_URL.to_string() + "/realms/dls/protocol/openid-connect/token";
-
-    let mut params = HashMap::new();
-    params.insert("client_id", "workflows-cli");
-    params.insert("grant_type", "refresh_token");
-    params.insert("refresh_token", refresh_token.as_str());
-
-    let res = reqwest::Client::new()
-        .post(token_url)
-        .form(&params)
-        .send()
-        .await
-        .expect("Failed to refresh token")
-        .json::<RefreshTokenResponse>()
-        .await
-        .unwrap();
-
-    let new_cached_tokens = CachedTokens {
-        id_token: res.access_token.clone(),
-        refresh_token: res.refresh_token.clone(),
-    };
-    fs::write(
-        path,
-        serde_json::to_string_pretty(&new_cached_tokens)
-            .expect("Unable to create new token object"),
-    )
-    .expect("Couldn't write to file");
-    res.access_token
+    refresh_using_auth_core(&tokens.refresh_token, &file_path).await
 }
