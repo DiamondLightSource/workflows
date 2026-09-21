67e1e6c2bcb45be1fc6f1318d85b22758abfe674
James Gilbert <james.gilbert@diamond.ac.uk>
2026-09-21 15:19:24 +0000

    refactor(frontend): rework task search to avoid seting state in useEffect

--- frontend/workflows-lib/lib/components/workflow/FuzzySearchBar.tsx
+++ frontend/workflows-lib/lib/components/workflow/FuzzySearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { TextField, InputAdornment } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -13,35 +13,14 @@ export const FuzzySearchBar: React.FC<SearchBarProps> = ({
   searchQuery,
   setSearchQuery,
 }) => {
-  const [localQuery, setLocalQuery] = useState(searchQuery);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setSearchQuery(localQuery);
-    }, 300);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [localQuery, setSearchQuery]);
-
-  useEffect(() => {
-    setLocalQuery(searchQuery);
-  }, [searchQuery]);
-
-  const handleClear = () => {
-    setLocalQuery("");
-    setSearchQuery("");
-  };
-
   return (
     <TextField
       aria-label="Search Outputs"
       variant="outlined"
       size="small"
-      value={localQuery}
+      value={searchQuery}
       onChange={(e) => {
-        setLocalQuery(e.target.value);
+        setSearchQuery(e.target.value);
       }}
       placeholder="Search by name or parent task"
       sx={{ width: "70%", marginRight: 0.5, color: "#737373" }}
@@ -55,11 +34,13 @@ export const FuzzySearchBar: React.FC<SearchBarProps> = ({
               <SearchIcon />
             </InputAdornment>
           ),
-          endAdornment: localQuery && (
+          endAdornment: searchQuery && (
             <InputAdornment position="end">
               <IconButton
                 aria-label="clear search"
-                onClick={handleClear}
+                onClick={() => {
+                  setSearchQuery("");
+                }}
                 edge="end"
                 size="small"
               >
